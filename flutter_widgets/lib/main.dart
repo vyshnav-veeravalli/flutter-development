9788f7355711a0e3b56af0aad2339def8756525c
vyshnav-veeravalli <vyshnavveeravalli2479@gmail.com>
2025-04-20 17:50:15 +0530

    eedho raasa le

--- flutter_widgets/lib/main.dart
+++ flutter_widgets/lib/main.dart
@@ -1,62 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main(){
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (_) => CounterModel(),
+    child: const myApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
 
-  Widget build(BuildContext context){
+class CounterModel extends ChangeNotifier{
+  int _count=0;
+  int get count => _count;
+
+  void increment(){
+    _count++;
+    notifyListeners();
+  }
+}
+
+class myApp extends StatelessWidget{
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Bank App UPI",
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-      ),
-      home: const HomeScreen(),
+      title: 'Provider Demo',
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: const ScreenOne(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget{
-  const HomeScreen({super.key});
+class ScreenOne extends StatelessWidget{
+  const ScreenOne({super.key});
 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<CounterModel>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HDBICI Bank Account'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          AccountCard(name: "Vyshnav", balance: 402015),
-          AccountCard(name: "Lakshmi", balance: 204561),
-          AccountCard(name: "Sailaja", balance: 189622),
-          AccountCard(name: "Sailuuu", balance: 753951),
-        ],
+      appBar: AppBar(title: const Text("rendu scrrenla counter"),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Counter: ${counter.count}", style: const TextStyle(fontSize: 60),),
+            const SizedBox(height: 16,width: 60,),
+            ElevatedButton(onPressed: counter.increment, child: const Text("penchuraa pumka"),),
+            const SizedBox(height: 16, width: 60,),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (_) => const ScreenTwo()),);
+            }, child: const Text("thadupari screen loki dhwaramu "),),],
+        ),
       ),
     );
   }
 }
 
 
-class AccountCard extends StatelessWidget{
-  final String name;
-  final double balance;
+class ScreenTwo extends StatelessWidget{
+  const ScreenTwo({super.key});
 
-  const AccountCard({super.key, required this.name, required this.balance});
-
-  Widget build(BuildContext context){
-    return Card(
-      margin: const EdgeInsets.all(24),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Text(
-          balance >= 200000
-            ? "$name has balance $balance"
-            : "$name needs min of 200000 (current balance: $balance)",
-          style: const TextStyle(fontSize: 18),
-        ),
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<CounterModel>(context);
+    return Scaffold(
+      appBar: AppBar(title: const Text("rendova thera"),),
+      body: Center(
+        child: Text("Shared Counter: ${counter.count}", style: const TextStyle(fontSize: 28),),
       ),
     );
   }
